d3badba2e4f50831424d03e8c01c649d53ab691b
robertvitoriano <robertvitorianodev@gmail.com>
2020-11-23 21:02:04 -0300

    style error modal

--- src/components/ErrorModal/index.js
+++ src/components/ErrorModal/index.js
@@ -1,4 +1,7 @@
-import React,{useState,useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
+import styles from 'styles.js'
+import Icon from 'react-native-vector-icons/FontAwesome';
+
 import {
   View,
   Text,
@@ -7,58 +10,47 @@ import {
   Dimensions,
 } from 'react-native';
 
-const ErrorModal = ({onClick,show}) => {
+const ErrorModal = ({onClick, show}) => {
+
+  const myButton = (
+    <Icon.Button
+      name="facebook"
+      backgroundColor="#3b5998"
+    >
+      Login with Facebook
+    </Icon.Button>
+  );
+  
+  const customTextButton = (
+    <Icon.Button name="facebook" backgroundColor="#3b5998" size={40}>
+      <Text style={{ fontFamily: 'Arial', fontSize: 15 }}>
+        Login with Facebook
+      </Text>
+    </Icon.Button>
+  );
+
 
   return (
-    
-     <>
-    {show?( <>
-      <View style={styles.opacity} />
-      <View style={styles.wrapper}>
-        <Text style={styles.warningText}>Ocorreu um erro</Text>
-        <TouchableOpacity style={styles.confirmButton} onPress={()=>onClick(false)}>
-          <Text>Ok</Text>
-        </TouchableOpacity>
-      </View>
-      </>
-      ):null}
+    <>
+      {show ? (
+        <>
+          <View style={styles.opacity} />
+          <View style={styles.wrapper}>
+            {customTextButton}
+            <Text style={styles.warningText}>Ocorreu um erro</Text>
+            <TouchableOpacity
+              style={styles.confirmButton}
+              onPress={() => onClick(false)}>
+              <Text>Ok</Text>
+            </TouchableOpacity>
+          </View>
+        </>
+      ) : null}
     </>
   );
 };
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
-const styles = StyleSheet.create({
-  wrapper: {
-    backgroundColor: 'aliceblue',
-    width: 350,
-    height: 220,
-    flexDirection: 'column',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    position: 'absolute',
-    zIndex: 1,
-    borderColor: '#907e70',
-    borderWidth: 4,
-  },
-  opacity: {
-    flex: 1,
-    height: windowHeight,
-    width: windowWidth,
-    opacity: 0.4,
-    backgroundColor: 'black',
-    position: 'absolute',
-  },
-  confirmButton:{
-    backgroundColor: '#e4dfc1',
-    width: 100,
-    borderRadius: 3,
-    height: 60,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  warningText:{
-    fontSize:22
-  }
-});
+
 
 export default ErrorModal;
